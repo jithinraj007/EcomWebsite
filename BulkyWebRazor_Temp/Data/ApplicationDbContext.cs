a2c6d73cbcc2c2325cd141e2c54311aba320513f
Jithinraj KJ <87666270+jithinraj007@users.noreply.github.com>
2025-07-05 20:12:40 +0530

    Configured connectionstring in BulkyWebRazor

---
+++ BulkyWebRazor_Temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BulkyWebRazor_Temp.Models;
+
+namespace BulkyWebRazor_Temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // DbSet properties go here
+        public DbSet<Category> Category { get; set; }
+    }
+}
