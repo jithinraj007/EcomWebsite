0308b263cee36b063dc7b7452399ad05c7bd89f9
Jithinraj KJ <87666270+jithinraj007@users.noreply.github.com>
2025-07-05 15:15:29 +0530

    Added Edit View

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -24,10 +24,30 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            if (obj.Name == obj.DisplayOrder.ToString()) { 
-            ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name."); 
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
             }
             _db.Category.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int Id) {
+            if (Id == 0 || Id < 1 || Id == null)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Category.Find(Id);//finds the category based on Id
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
